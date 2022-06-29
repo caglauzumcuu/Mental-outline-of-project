50c5264c6bf9efc93c2879d79fd9f7dded8a70a0
caglauzumcuu <cagla.uzumcu@std.yildiz.edu.tr>
2022-06-30 00:59:25 +0300

    first react project
    my first react project

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import reactLogo from "../images/react-icon-small.png"
+
+
+function Navbar() {
+  return (
+    <nav>
+         <img src={reactLogo} className="nav--icon" />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">Çağla's Projects - Project 1</h4>
+    </nav>
+  )
+}
+
+export default Navbar
